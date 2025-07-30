23c101b9e7a26fb58b9c9402b25442044c6c2055
Tay Huval <tay.huval@gmail.com>
2025-07-30 21:44:55 +0000

    let's slow the animation even more; I want to watch the card move from t

--- src/components/game/card.tsx
+++ src/components/game/card.tsx
@@ -83,7 +83,7 @@ export function Card({ card, isSelected, isStacked, className, onClick, draggabl
       style={style}
       className={cn(
         cardSize,
-        'rounded-md bg-card border-2 border-black cursor-pointer relative p-0.5 sm:p-1 flex flex-col justify-between transition-all duration-300 ease-in-out overflow-hidden',
+        'rounded-md bg-card border-2 border-black cursor-pointer relative p-0.5 sm:p-1 flex flex-col justify-between transition-all duration-[1300ms] ease-in-out overflow-hidden',
         ringClass,
         suitColorClass,
         className,
