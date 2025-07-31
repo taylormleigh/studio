f6ca0d78dec57c2ca3cba7a42d2ab7a9e7a730ba
Tay Huval <tay.huval@gmail.com>
2025-07-31 01:52:32 +0000

    I'm not seeing my favicon showing up correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,6 +31,8 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#E0E0E0" />
 
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
