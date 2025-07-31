e019e4a7f891a671afb59067a2741b40fef1a5a9
Tay Huval <tay.huval@gmail.com>
2025-07-31 05:19:47 +0000

    in the small view of the cards the bottom number is overflowing off of t

--- src/components/game/card.tsx
+++ src/components/game/card.tsx
@@ -92,7 +92,7 @@ export function Card({ card, isSelected, isHighlighted, className, onClick, drag
         <div className="self-center">
           <SuitIcon suit={card.suit} className="text-xl" />
         </div>
-        <div className="flex items-center self-end rotate-180">
+        <div className="hidden sm:flex items-center self-end rotate-180">
           <div className="text-lg font-bold leading-none">{card.rank}</div>
         </div>
       </div>
