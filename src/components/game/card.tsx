5bb95d76997e2448267a8c185594dc3e42e83f02
Tay Huval <tay.huval@gmail.com>
2025-08-02 05:55:50 +0000

    comments on card styling

--- src/components/game/card.tsx
+++ src/components/game/card.tsx
@@ -78,16 +78,23 @@ export function Card({ card, isSelected, isHighlighted, className, onClick, drag
         )}
       >
         <div className="flex items-center gap-1">
-          <div className="sm:text-sm md:text-md font-bold leading-none">{card.rank}</div>
-          {isStacked && <SuitIcon suit={card.suit} className="text-lg leading-none" />}
+          {/* stacked face up card rank */}
+          <div className="sm:text-sm md:text-md font-bold leading-none">
+            {card.rank}
+          </div>
+
+          {/* stacked face up card suite icon */}
+          {isStacked && <SuitIcon suit={card.suit} className="text-lg sm:text-md " />}
         </div>
 
         {!isStacked && (
           <>
+            {/* top card suite icon */}
             <div className="self-center flex">
               <SuitIcon suit={card.suit} className="text-2xl" />
             </div>
             <div>
+              {/* lower right-hand rank */}
               <div className="hidden xs:hidden md:flex self-end rotate-180">
                 <div className="sm:text-md md:text-md font-bold leading-none">{card.rank}</div>
               </div>
