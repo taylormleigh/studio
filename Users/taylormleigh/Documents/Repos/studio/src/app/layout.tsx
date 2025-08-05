142def2d45eb073921fca1ee98fa0515a19f4b98
Tay Huval <tay.huval@gmail.com>
2025-08-05 19:59:36 +0000

    the change still isn't doing what we want it to do in addition to totall

--- Users/taylormleigh/Documents/Repos/studio/src/app/layout.tsx
+++ Users/taylormleigh/Documents/Repos/studio/src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#E0E0E0" />
         
+        <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
       </head>
       <body className="font-body antialiased bg-background">
