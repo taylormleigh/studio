fdaf460d768a75c12a695a0e4dfda0b451e77f9b
Tay Huval <tay.huval@gmail.com>
2025-08-05 20:33:51 +0000

    next-pwa to package

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,7 @@ const pwaConfig = {
   dest: 'public',
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 };
 
 // Ensure PWA is only enabled for production builds
