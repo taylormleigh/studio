b0c4ca1b711182f723cbec4ea1136404c38be6ad
Tay Huval <tay.huval@gmail.com>
2025-08-05 20:06:36 +0000

    testing w new manifest references

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import { Toaster } from "@/components/ui/toaster"
 export const metadata: Metadata = {
   title: 'Deck of Cards',
   description: 'Simple card games for web and mobile',
-  manifest: '/manifest.json',
+  manifest: '../public/manifest.json',
   icons: {
     icon: '/favicon.svg',
     apple: '/favicon.svg',
@@ -30,9 +30,8 @@ export default function RootLayout({
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
         <meta name="msapplication-TileColor" content="#E0E0E0" />
         <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content="#E0E0E0" />
         
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="../public/manifest.json" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" sizes="any" />
       </head>
       <body className="font-body antialiased bg-background">
